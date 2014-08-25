8120994259af30c8349d9ed0113350c9c1991dd0
uohzoaix <johnchou66@gmail.com>
2014-08-25 10:24:55 +0800

    修改word2phrases

--- src/com/lauty/w2v/Word2Phrases.java
+++ src/com/lauty/w2v/Word2Phrases.java
@@ -6,10 +6,12 @@ import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.Arrays;
-
-import com.lauty.w2v.Word2Vec.VocabWord;
+import java.util.Comparator;
+import java.util.List;
 
 public class Word2Phrases {
 
@@ -20,10 +22,9 @@ public class Word2Phrases {
 	private static VocabWord[] vocabs;
 	private static int[] vocabHash;
 	private static int debug_mode = 2, min_count = 5, min_reduce = 1;
-	private static int vocabMaxSize = 10000, vocab_size = 0;
-	private static long train_words = 0;
+	private static int vocabMaxSize = 10000, vocabSize = 0;
+	private static long trainWords = 0;
 	private static float threshold = 100;
-	private static long next_random = 1;
 
 	public static class VocabWord {
 		Integer cn = 0;
@@ -106,10 +107,142 @@ public class Word2Phrases {
 		trainModel();
 	}
 
-	public static void trainModel() throws FileNotFoundException {
-		long pa = 0, pb = 0, pab = 0, oov, i, li = -1, cn = 0;
+	// Returns hash value of a word
+	public static int getWordHash(String word) {
+		int a;
+		BigInteger hash = BigInteger.ZERO;
+		byte[] wordBytes = word.getBytes();
+		for (a = 0; a < wordBytes.length; a++) {
+			if (wordBytes[a] != 0) {
+				hash = hash.multiply(new BigInteger(257 + "")).add(new BigInteger(wordBytes[a] + ""));
+			}
+		}
+		hash = hash.mod(new BigInteger(VOCAB_HASH_SIZE + ""));
+		return hash.intValue();
+	}
 
-		//char word[MAX_STRING], last_word[MAX_STRING], bigram_word[MAX_STRING * 2];
+	// Returns position of a word in the vocabulary; if the word is not found, returns -1
+	public static int searchVocab(String word) {
+		int hash = getWordHash(word);
+		while (true) {
+			try {
+				if (vocabHash[hash] == -1)
+					return -1;
+				try {
+					if (word.equals(vocabs[vocabHash[hash]].getWord())) {
+						return vocabHash[hash];
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				hash = (hash + 1) % VOCAB_HASH_SIZE;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// Adds a word to the vocabulary
+	public static int addWordToVocab(String word) {
+		int hash = 0, length = word.length();
+		if (length > MAX_STRING)
+			length = MAX_STRING;
+		word = word.substring(0, length);
+		vocabs[vocabSize].setWord(word);
+		vocabs[vocabSize].setCn(0);
+		vocabSize++;
+		// Reallocate memory if needed
+		if (vocabSize + 2 >= vocabMaxSize) {
+			vocabMaxSize += 1000;
+			VocabWord[] newvocabs = new VocabWord[vocabMaxSize];
+			System.arraycopy(vocabs, 0, newvocabs, 0, vocabMaxSize - 1000);
+			for (int i = vocabMaxSize - 1000; i < vocabMaxSize; i++) {
+				newvocabs[i] = new VocabWord();
+			}
+			vocabs = newvocabs;
+			//vocabs = (struct vocab_word *)realloc(vocab, vocab_max_size * sizeof(struct vocab_word));
+		}
+		hash = getWordHash(word);
+		while (vocabHash[hash] != -1)
+			hash = (hash + 1) % VOCAB_HASH_SIZE;
+		vocabHash[hash] = vocabSize - 1;
+		return vocabSize - 1;
+	}
+
+	public int vocabCompare(VocabWord a, VocabWord b) {
+		return b.getCn() - a.getCn();
+	}
+
+	// Sorts the vocabulary by frequency using word counts
+	public static void sortVocab() {
+		List<VocabWord> vocabList = Arrays.asList(vocabs);
+		vocabList.remove(null);
+		vocabs = (VocabWord[]) vocabList.toArray();
+		int a, size;
+		int hash;
+		// Sort the vocabulary and keep </s> at the first position
+		Comparator<VocabWord> comparator = new Comparator<Word2Phrases.VocabWord>() {
+			@Override
+			public int compare(VocabWord o1, VocabWord o2) {
+				return o2.getCn() - o1.getCn();
+			}
+		};
+		Arrays.sort(vocabs, 1, vocabs.length, comparator);
+		for (a = 0; a < VOCAB_HASH_SIZE; a++)
+			vocabHash[a] = -1;
+		size = vocabSize;
+		trainWords = 0;
+		for (a = 0; a < size; a++) {
+			// Words occuring less than min_count times will be discarded from the vocab
+			if (vocabs[a].getCn() < min_count) {
+				vocabSize--;
+				vocabs[vocabSize].setWord("");
+			} else {
+				// Hash will be re-computed, as after the sorting it is not actual
+				hash = getWordHash(vocabs[a].getWord());
+				while (vocabHash[hash] != -1)
+					hash = (hash + 1) % VOCAB_HASH_SIZE;
+				vocabHash[hash] = a;
+				trainWords += vocabs[a].getCn();
+			}
+		}
+		//vocabs = new VocabWord[vocabSize + 1];
+		// vocab = (struct vocab_word *)realloc(vocab, (vocab_size + 1) * sizeof(struct vocab_word));
+		// Allocate memory for the binary tree construction
+		//		for (a = 0; a < vocabSize; a++) {
+		//			vocabs[a].setCode(new byte[MAX_CODE_LENGTH]);
+		//			vocabs[a].setPoint(new int[MAX_CODE_LENGTH]);
+		//		}
+	}
+
+	// Reduces the vocabulary by removing infrequent tokens
+	public static void reduceVocab() {
+		int a, b = 0;
+		int hash;
+		for (a = 0; a < vocabSize; a++)
+			if (vocabs[a].cn > min_reduce) {
+				vocabs[b].setCn(vocabs[a].getCn());
+				vocabs[b].setWord(vocabs[a].getWord());
+				b++;
+			} else
+				vocabs[a].setWord("");
+		vocabSize = b;
+		for (a = 0; a < VOCAB_HASH_SIZE; a++)
+			vocabHash[a] = -1;
+		for (a = 0; a < vocabSize; a++) {
+			// Hash will be re-computed, as it is not actual
+			hash = getWordHash(vocabs[a].getWord());
+			while (vocabHash[hash] != -1)
+				hash = (hash + 1) % VOCAB_HASH_SIZE;
+			vocabHash[hash] = a;
+		}
+		min_reduce++;
+	}
+
+	public static void trainModel() throws FileNotFoundException {
+		int pa = 0, pb = 0, pab = 0, oov, i, li = -1, cn = 0;
+
+		String last_word = "", bigram_word = "";
 		float score;
 		System.out.println(String.format("Starting training using file %s", trainFile));
 		learnVocabFromTrainFile();
@@ -118,15 +251,126 @@ public class Word2Phrases {
 		try {
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(trainFile)));
 			dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(outputFile)));
+			String tmp = null;
+			while ((tmp = br.readLine()) != null) {
+				String[] words = tmp.split(" ");
+				int len = 0;
+				for (String word : words) {
+					last_word = len == 0 ? "" : word;
+					len++;
+					if (word.equals("</s>")) {
+						dos.writeUTF("\n");
+						continue;
+					}
+					cn++;
+					if ((debug_mode > 1) && (cn % 100000 == 0)) {
+						System.out.printf("Words written: %dK%c", cn / 1000, 13);
+					}
+					oov = 0;
+					i = searchVocab(word);
+					if (i == -1)
+						oov = 1;
+					else
+						pb = vocabs[i].getCn();
+					if (li == -1)
+						oov = 1;
+					li = i;
+					bigram_word = last_word + "_" + word;
+					i = searchVocab(bigram_word);
+					if (i == -1)
+						oov = 1;
+					else
+						pab = vocabs[i].getCn();
+					if (pa < min_count)
+						oov = 1;
+					if (pb < min_count)
+						oov = 1;
+					if (oov > 0)
+						score = 0;
+					else
+						score = (pab - min_count) / (float) pa / (float) pb * (float) trainWords;
+					if (score > threshold) {
+						dos.writeUTF("_" + word);
+						pb = 0;
+					} else
+						dos.writeUTF(word);
+					pa = pb;
+				}
+			}
 		} catch (Exception e) {
+		} finally {
+			try {
+				if (dos != null)
+					dos.close();
+				if (br != null)
+					br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
-	public static void learnVocabFromTrainFile() throws FileNotFoundException {
-		String[] word = new String[MAX_STRING], last_word = new String[MAX_STRING], bigram_word = new String[MAX_STRING * 2];
+	public static void learnVocabFromTrainFile() {
+		String last_word = "", bigram_word = "";
 		int a, i, start = 1;
 		for (a = 0; a < VOCAB_HASH_SIZE; a++)
 			vocabHash[a] = -1;
-		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(trainFile)));
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(trainFile)));
+			String tmp;
+			while ((tmp = br.readLine()) != null) {
+				String words[] = tmp.split(" ");
+				trainWords += words.length;
+				if (debug_mode > 0 && trainWords % 100000 == 0) {
+					System.out.printf("%dK %d%c", trainWords / 1000, vocabSize, 13);
+				}
+				for (String word : words) {
+					if (word.equals("</s>")) {
+						start = 1;
+						continue;
+					} else {
+						start = 0;
+					}
+					i = searchVocab(word);
+					if (i == -1) {
+						try {
+							a = addWordToVocab(word);
+							vocabs[a].setCn(1);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					} else
+						vocabs[i].setCn(vocabs[i].getCn() + 1);
+					if (start > 0) {
+						continue;
+					}
+					bigram_word = last_word + "_" + word;
+					last_word = word;
+					i = searchVocab(bigram_word);
+					if (i == -1) {
+						a = addWordToVocab(bigram_word);
+						vocabs[a].setCn(1);
+					} else
+						vocabs[i].setCn(vocabs[i].getCn() + 1);
+					if (vocabSize > VOCAB_HASH_SIZE * 0.7)
+						reduceVocab();
+				}
+			}
+			sortVocab();
+			if (debug_mode > 0) {
+				System.out.println(String.format("Vocab size (unigrams + bigrams): %d\n", vocabSize));
+				System.out.println(String.format("Words in train file: %d\n", trainWords));
+			}
+		} catch (Exception e) {
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
